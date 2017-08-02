e1d1e2e10de502e4fe2a30c29acd444285c90427
Brett Swiecicki <brettcs@umich.edu>
2017-08-02 03:56:26 -0400

    Playthrough simulator should be totally completed. Just need to debug it and add it as a feature to the rest of the program.

--- RouletteStrategy/PlaythroughSimulator.h
+++ RouletteStrategy/PlaythroughSimulator.h
@@ -3,6 +3,8 @@
 #define PLAYTHROUGHSIMULATOR_H
 
 #include <iostream>
+#include <random>
+#include <time.h>
 #include <vector>
 using namespace std;
 
@@ -17,7 +19,12 @@ public:
 	}
 
 	void runSimulations() {
-
+		clock_t t = clock(); //Start clock
+		cout << endl;
+		cout << "Currently running " << total_sims << " simulations..." << endl;
+		//Run sims
+		printRuntime(t);
+		//Print results
 	}
 
 	void updateParameters() {
@@ -34,10 +41,13 @@ public:
 
 private:
 	vector<double> strat_stakes;
+	vector<double> final_amounts;
 	int total_rolls;
 	int board_size;
 	int board_hits;
 	int total_sims;
+	int num_busts;
+	int num_finishes;
 	double payout_factor;
 	double starting_bankroll;
 	double total_bonus;
@@ -45,8 +55,78 @@ private:
 	double bet_contribution_rate;
 
 	void slycickSim() {
+		std::random_device rd;  //Will be used to obtain a seed for the random number engine
+		std::mt19937 engine(rd()); //Standard mersenne_twister_engine seeded with rd()
+		std::uniform_int_distribution<> dist(0, (board_size - 1));
 
-	}
+		num_busts = 0;
+		num_finishes = 0;
+		final_amounts.clear();
+
+		double min_bankroll = 0.0;
+		for (int i = 0; i < (int)strat_stakes.size(); ++i) {
+			min_bankroll += strat_stakes[i];
+		}
+		double total_required_playthrough = playthrough_rec_factor * total_bonus;
+
+		int sim_count = 0;
+		while (sim_count < total_sims) {
+			double dynamic_bankroll = starting_bankroll;
+			double lower_bound = starting_bankroll - min_bankroll;
+			double upper_bound = starting_bankroll + min_bankroll;
+			double dynamic_playthrough = 0.0;
+			int roll_on_strat = 0;
+			double bet;
+
+			while ((dynamic_bankroll > 0) && (dynamic_playthrough < total_required_playthrough)) {
+				int random_num = dist(engine);
+				if (strat_stakes[roll_on_strat] <= (dynamic_bankroll - lower_bound)) {
+					bet = strat_stakes[roll_on_strat];
+				}
+				else {
+					bet = (dynamic_bankroll - lower_bound);
+				}
+				dynamic_bankroll -= bet;
+				dynamic_playthrough += (bet * (bet_contribution_rate / 100.0));
+
+				if (random_num < board_hits) { //HIT!
+					dynamic_bankroll += ((payout_factor + 1) * bet);
+					roll_on_strat = 0;
+				}
+				else { //MISS!
+					++roll_on_strat;
+					if (roll_on_strat == (int)strat_stakes.size()) {
+						roll_on_strat = 0;
+					}
+				}
+
+				//Check bounds here
+				if (dynamic_bankroll >= upper_bound) {
+					upper_bound = dynamic_bankroll + min_bankroll;
+					lower_bound = dynamic_bankroll - min_bankroll;
+					roll_on_strat = 0;
+				}
+				else if (dynamic_bankroll == lower_bound) {
+					upper_bound = dynamic_bankroll + min_bankroll;
+					lower_bound = dynamic_bankroll - min_bankroll;
+					if (lower_bound < 0) {
+						lower_bound = 0;
+					}
+					roll_on_strat = 0;
+				}
+
+			} // End of simulation
+
+			if (dynamic_bankroll <= 0.0) {
+				++num_busts;
+			}
+			else {
+				++num_finishes;
+				final_amounts.push_back(dynamic_bankroll);
+			}
+			++sim_count;
+		}
+	} // End of sylcickiSim
 
 	void getRolls() {
 		cout << "Enter how many rolls your strategy can have before breaking: ";
@@ -126,6 +206,19 @@ private:
 	void getSimulationCount() {
 		cout << "Please enter the total number of simulations you would like to run: ";
 		cin >> total_sims;
+	}
+
+	void printRuntime(clock_t& t) {
+		t = clock() - t;
+		double seconds = (((float)t) / (CLOCKS_PER_SEC));
+		double hours = (((seconds) / 60.0) / 60.0);
+		seconds -= (((int)hours) * 3600);
+		double minutes = ((seconds) / 60.0);
+		seconds -= (((int)minutes) * 60);
+		cout << "Total running time: ";
+		cout << (int)hours << " Hours: ";
+		cout << (int)minutes << " Minutes: ";
+		cout << (int)seconds << " Seconds" << endl;
 	}
 	
 };
